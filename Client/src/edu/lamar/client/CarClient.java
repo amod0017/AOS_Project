48298b4f33482ad7b90dce7a413bbc49ccbb873b
amod0017 <amod0017@gmail.com>
2016-03-29 10:42:49 -0500

    Added Client Server Framework

---
+++ Client/src/edu/lamar/client/CarClient.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package edu.lamar.client;
+
+/**
+ * @author user
+ *
+ */
+public class CarClient extends AbstractClient {
+
+	public CarClient(String host, int port) {
+		super(host, port);
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.lamar.client.AbstractClient#handleMessageFromServer(java.lang.Object)
+	 */
+	@Override
+	protected void handleMessageFromServer(Object msg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
